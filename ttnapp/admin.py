73ec36c4a6e30fbe1d3122c19f67f42e39b0507f
rh2975 <rahat2975134@gmail.com>
2021-02-26 19:26:04 +0600

    sec commit

--- ttnapp/admin.py
+++ ttnapp/admin.py
@@ -6,8 +6,24 @@ from .models import Teacher, Student, Subject, Person, Exam
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("name",)
 
-admin.site.register(Teacher)
-admin.site.register(Student)
-admin.site.register(Subject)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ["get_name",]
+    def get_name(self, obj):
+        return obj.details.name
+
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["get_name",]
+    def get_name(self, obj):
+        return obj.details.name
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Exam)
+admin.site.register(Exam, ExamAdmin)
